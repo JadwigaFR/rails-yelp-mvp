ae8af9e3c9512ea49f2521ce6fa35df4ba10bc7d
Jadwiga Coumert <jadwiga.coumert@gmail.com>
2018-11-14 15:30:06 +0100

    Defined the yelp for restaurants

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, :dependent => :destroy
+  # before_validation { self.category = self.category.downcase }
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+  # validates :category, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+chupenga = Restaurant.new(name: 'Chupenga', category: 'italian', address: 'Checkpoint Charlie')
+chupenga.save!
+burgermeister = Restaurant.new(name: 'Burgermeister', category: 'japanese', address: 'Kreuzberg')
+burgermeister.save!
+schnitzel = Restaurant.new(name: 'Schnitzel Haus', category: 'french', address: 'Aubing')
+schnitzel.save!
+hofbrauhaus = Restaurant.new(name: 'Hofbrauhaus', category: 'belgian', address: 'Munich')
+hofbrauhaus.save!
+sushi = Restaurant.new(name: 'Sushi Yummy', category: 'chinese', address: 'Berlin')
+sushi.save!
